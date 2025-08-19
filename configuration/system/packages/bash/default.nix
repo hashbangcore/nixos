5ae9e2f2faa03a37e87feb0b63eb5f2fa16fc3e4
Hash <hashbangcore@duck.com>
2025-08-19 06:21:27 -0400

    feat: include Bash configuration

---
+++ configuration/system/packages/bash/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  programs.bash = {
+    promptInit = ''
+      # Ignora comandos que comienzan con espacio
+      HISTCONTROL=ignorespace
+
+      # Comandos que no se guardan en historial
+      #HISTIGNORE='age*:ssh*:gocryptfs*'
+
+      # Formato de fecha/hora en historial (día-mes hora:minuto)
+      HISTTIMEFORMAT="%d-%m %H:%M "
+
+      # Autocorrección para rutas en 'cd'
+      shopt -s cdspell
+
+      # Autocorrección en autocompletado de directorios
+      shopt -s dirspell
+
+      # Autocompletado insensible a mayúsculas
+      shopt -s nocaseglob
+
+      # Desactiva colores en listados de autocompletado
+      #bind 'set colored-stats off'
+
+      # Desactiva resaltado de prefijos al autocompletar
+      #bind 'set colored-completion-prefix off'
+
+      # Muestra opciones con un solo TAB
+      bind 'set show-all-if-ambiguous on'
+
+      # Autocompletado que ignora mayúsculas/minúsculas
+      bind 'set completion-ignore-case on'
+
+
+
+      if [ "$(id -u)" -eq 0 ]; then
+        PS1='\[\e[31m\]#!  \[\e[0m\]'
+      else
+        PS1='#!  '
+      fi
+
+    '';
+  };
+}
