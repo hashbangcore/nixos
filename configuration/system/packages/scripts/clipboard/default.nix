9107cbafe06e21276843a3142ee4b028131826de
Hash <hashbangcore@duck.com>
2025-08-19 06:59:49 -0400

    feat: add clipboard script

---
+++ configuration/system/packages/scripts/clipboard/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+let
+  dependencies = with pkgs; [
+    xclip
+    wl-clipboard
+  ];
+in
+{
+  environment.systemPackages = with pkgs; [
+    (writeShellApplication {
+      name = "clip-copy";
+      runtimeInputs = dependencies;
+      text = ''
+        if [ -n "$WAYLAND_DISPLAY" ]; then
+          wl-copy
+        else
+          xclip -selection clipboard
+        fi
+      '';
+      meta.description = "Copy from stdin to clipboard using xclip or wl-clipboard on Wayland";
+    })
+
+    (writeShellApplication {
+      name = "clip-paste";
+      runtimeInputs = dependencies;
+      text = ''
+        if [ -n "$WAYLAND_DISPLAY" ]; then
+          wl-paste
+        else
+          xclip -o -selection clipboard
+        fi
+      '';
+      meta.description = "Paste from clipboard to stdout using xclip or wl-clipboard on Wayland";
+    })
+  ];
+}
