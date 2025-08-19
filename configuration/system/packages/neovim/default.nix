1d5de5630380100c6257c7a0eb3456192e403483
Hash <hashbangcore@duck.com>
2025-08-19 06:33:17 -0400

    feat: include Neovim configuration

---
+++ configuration/system/packages/neovim/default.nix
@@ -0,0 +1,91 @@
+{
+  pkgs,
+  colorscheme,
+  ...
+}:
+let
+  theme = colorscheme.neovim;
+in
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    withPython3 = true;
+    withNodeJs = true;
+    withRuby = true;
+    configure = {
+      customRC = ''
+            set backupcopy=no
+            set number
+            set list
+            "set listchars=tab:␉\ ,trail:⋅,nbsp:⍽
+            "set listchars=tab:⭾\ ,trail:¤,nbsp:•
+            set listchars=tab:›\ ,trail:·,nbsp:␣
+            set expandtab
+            set tabstop=2 shiftwidth=2
+            set scrolloff=50
+            set cursorline
+            set signcolumn=yes
+            set nowrap
+            set termguicolors
+            colorscheme ${theme}
+            set signcolumn=no
+            set mouse=
+            inoremap <C-l> <C-o>$
+            inoremap <C-h> <C-o>^
+            nmap <space>e <Cmd>CocCommand explorer<CR>
+            "autocmd BufWritePre *.nix :silent !alejandra %
+
+            if has("autocmd")
+              autocmd BufReadPost *
+              \ if line("'\'") > 0 && line("'\'") <= line("$") |
+              \ exe "normal g`\"" |
+              \ endif
+            endif
+
+
+            " Navegar entre sugerencias de autocompletado
+          inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+          inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+
+        " Confirmar con Enter
+          inoremap <expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+      '';
+
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [
+          #coc-ultisnips
+          #codesnap-nvim
+          #nvchad
+          vim-colorschemes
+          coc-css
+          coc-emmet
+          coc-explorer
+          coc-go
+          coc-html
+          coc-json
+          coc-markdownlint
+          coc-nvim
+          coc-pairs
+          coc-pyright
+          coc-rls
+          coc-sh
+          coc-spell-checker
+          coc-svelte
+          coc-tsserver
+          coc-yank
+          #llm-nvim
+          lush-nvim
+          melange-nvim
+          nerdcommenter
+          ollama-nvim
+          srcery-vim
+          #telescope-nvim
+          vim-just
+          vim-nix
+        ];
+      };
+    };
+  };
+}
