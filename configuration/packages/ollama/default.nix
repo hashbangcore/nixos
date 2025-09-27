102c3fcc5a37dbfcd89dd1c6cd75d36a1eaf50c7
Hash <hashbangcore@duck.com>
2025-09-26 23:01:51 -0300

    package: remove alpaca and blueprint-compiler
    Remove alpaca and blueprint-compiler package definitions
    and their references from environment.systemPackages
    The ollama service configuration remains enabled in the
    repository with port 11434

--- configuration/packages/ollama/default.nix
+++ configuration/packages/ollama/default.nix
@@ -1,13 +1,4 @@
-{ pkgs, unstable, ... }:
-let
-  blueprint-compiler = unstable.callPackage ./blueprinter/package.nix { };
-  alpaca-local = unstable.callPackage ./alpaca/package.nix { };
-  alpaca-with-blueprint = alpaca-local.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs ++ [ blueprint-compiler ];
-  });
-in
 {
-
   environment.etc = {
     "bash_completion.d/ollama-completion".source = ./ollama-completion.bash;
   };
@@ -16,9 +7,4 @@ in
     enable = true;
     port = 11434;
   };
-
-  environment.systemPackages = [
-    pkgs.gollama
-    alpaca-with-blueprint
-  ];
 }
